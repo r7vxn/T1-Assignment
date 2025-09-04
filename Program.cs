5560e765f9602ecf80260336366100f6e347c17e
Moses Ma <701000372@GoTVDSB.ca>
2025-09-04 09:36:56 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+String Mstring = @"
+     M     M
+    M M   M M
+   M   M M   M
+  M     M     M
+ M             M
+M               M";
+Console.Write(Mstring);  
+Console.Write(Mstring);

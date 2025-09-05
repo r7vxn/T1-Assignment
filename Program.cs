24cb46bd1829bc9b896fa4c09d470e1549c688f5
Moses Ma <701000372@GoTVDSB.ca>
2025-09-05 08:44:10 -0400

    I made a space base :3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, my name is Moses Ma\n");
+Console.WriteLine("Comment is for other coders to know what to debug, or know where they left off\n");
+
+
+/*
+ *   .             *        .     .       .
+       .     _     .     .            .       .
+.    .   _  / |      .        .  *         _  .     .
+        | \_| |                           | | __
+      _ |     |                   _       | |/  |
+     | \      |      ____        | |     /  |    \
+     |  |     \    +/_\/_\+      | |    /   |     \
+   _/____\--...\___ \_||_/ ___...|__\-..|____\____/__
+      .     .      |_|__|_|         .       .
+   .    . .       _/ /__\ \_ .          .
+      .       .    .           .         .
+*/
+// This is a Base on the Moon
+
+
+
+
+
+
+
+
+
+
+
+
+
 String Mstring = @"
      M     M
     M M   M M
